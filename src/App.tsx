2b93f10c14dd6d5f92e1b5cad828a59425d94881
Gustavo <gustavoc.moraes@hotmail.com>
2021-02-07 19:24:30 -0300

    Login validation

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 import GlobalStyles from './styles/global';
 
-// import SignIn from './pages/SignIn/index';
-import SignUp from './pages/SignUp/index';
+import SignIn from './pages/SignIn/index';
+// import SignUp from './pages/SignUp/index';
 
 const App: React.FC = () => {
   return (
     <>
-      <SignUp />
+      <SignIn />
       <GlobalStyles />
     </>
   );
--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,6 +1,12 @@
-import React from 'react';
+/* eslint-disable no-unused-expressions */
+import React, { useRef, useCallback } from 'react';
+
+import { Form } from '@unform/web';
+import { FormHandles } from '@unform/core';
 
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+
+import * as Yup from 'yup';
 
 import logoImg from '../../assets/logo.svg';
 
@@ -8,36 +14,62 @@ import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 import { Container, Content, Background } from './styles';
+import getValidationErrors from '../../utils/getValidationErrors';
 
-const SignIn: React.FC = () => (
-  <Container>
-    <Content>
-      <img src={logoImg} alt="logo" />
+const SignIn: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
 
-      <form>
-        <h1>Login</h1>
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      formRef.current?.setErrors({});
 
-        <Input icon={FiMail} name="e-mail" placeholder="E-mail" />
-        <Input
-          icon={FiLock}
-          name="password"
-          type="password"
-          placeholder="Password"
-        />
+      const schema = Yup.object().shape({
+        email: Yup.string()
+          .required('E-mail required')
+          .email('Enter a valid e-mail adress'),
+        password: Yup.string().required('At least 6 digits'),
+      });
 
-        <Button type="submit">Sign In </Button>
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (err) {
+      const errors = getValidationErrors(err);
 
-        <a href="forgot">Forgot password</a>
-      </form>
+      formRef.current?.setErrors(errors);
+    }
+  }, []);
 
-      <a href="create">
-        Create account
-        <FiLogIn />
-      </a>
-    </Content>
+  return (
+    <Container>
+      <Content>
+        <img src={logoImg} alt="logo" />
 
-    <Background />
-  </Container>
-);
+        <Form ref={formRef} onSubmit={handleSubmit}>
+          <h1>Login</h1>
+
+          <Input icon={FiMail} name="email" placeholder="E-mail" />
+          <Input
+            icon={FiLock}
+            name="password"
+            type="password"
+            placeholder="Password"
+          />
+
+          <Button type="submit">Sign In </Button>
+
+          <a href="forgot">Forgot password</a>
+        </Form>
+
+        <a href="create">
+          Create account
+          <FiLogIn />
+        </a>
+      </Content>
+
+      <Background />
+    </Container>
+  );
+};
 
 export default SignIn;
